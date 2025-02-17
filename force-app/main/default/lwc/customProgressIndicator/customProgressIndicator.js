79bbc9cc5f9d75c966dabbf18a1cc415af09b94a
Barouch Kandov <Barouchk@bezeqint.co.il>
2025-02-17 16:27:35 +0200

    add dynamic and progress indicator

--- force-app/main/default/lwc/customProgressIndicator/customProgressIndicator.js
+++ force-app/main/default/lwc/customProgressIndicator/customProgressIndicator.js
@@ -2,7 +2,8 @@
  * Created by bkandov on 29/12/2020.
  */
 
-import {LightningElement, api} from 'lwc';
+import { LightningElement, api } from 'lwc';
+import userLanguage from '@salesforce/i18n/lang';
 
 export default class CustomProgressIndicator extends LightningElement {
 
@@ -13,5 +14,32 @@ export default class CustomProgressIndicator extends LightningElement {
     @api hasError = false
 
     @api steps
+    _steps = []
+
+    completePrecent = 0
+    markerClass = 'slds-progress__marker'
+
+    connectedCallback() {
+        if (userLanguage === 'he') {
+            this.markerClass += ' marker__heb'
+        }
+
+        if (this.steps) {
+            this._steps = this.steps.map((step) => ({
+                ...step,
+                stepClass: this.currentStep && this.currentStep === step.value ? 'slds-progress__item slds-is-active' : 'slds-progress__item'
+            }))
+        }
+
+        if (this.currentStep) {
+            for (let step of this._steps) {
+                if (step.value !== this.currentStep) {
+                    step.stepClass += ' slds-is-completed'
+                } else {
+                    break;
+                }
+            }
+        }
+    }
 
 }
