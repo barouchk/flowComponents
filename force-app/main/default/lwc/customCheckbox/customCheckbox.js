b33a03ba4fd90fbd4f8e8382db85d87d047a9bb7
Barouch Kandov <Barouchk@bezeqint.co.il>
2025-04-08 14:11:55 +0300

    add flow validation

--- force-app/main/default/lwc/customCheckbox/customCheckbox.js
+++ force-app/main/default/lwc/customCheckbox/customCheckbox.js
@@ -3,7 +3,7 @@
  */
 
 import { LightningElement, api } from 'lwc';
-import { loadStyle } from 'lightning/platformResourceLoader'; 
+import { loadStyle } from 'lightning/platformResourceLoader';
 // import customCheckboxStyle from '@salesforce/resourceUrl/CustomCheckboxStyle';
 import { TYPES, MAX_OPTIONS_PER_COLUMN } from "./constants"
 export default class CustomCheckbox extends LightningElement {
@@ -15,10 +15,10 @@ export default class CustomCheckbox extends LightningElement {
     @api label;
 
     @api get value() {
-        return this.__value; 
+        return this.__value;
     }
     set value(value) {
-        this.__value = value ?? (this.type === TYPES.CHECKBOX ? []:'');
+        this.__value = value ?? (this.type === TYPES.CHECKBOX ? [] : '');
     }
     @api placeholder = '';
 
@@ -36,43 +36,43 @@ export default class CustomCheckbox extends LightningElement {
     @api strSelectedValues;
 
     // options values as array of string such as ['a', 'b' .... ] 
-    @api get listOfStrings() {}
+    @api get listOfStrings() { }
     set listOfStrings(values) {
         this.options = this.generateOptions(values);
     }
 
     // options values as string values delimited by 'stringSpliter'
-    @api get stringOfValues() {}
+    @api get stringOfValues() { }
     set stringOfValues(values) {
-        this.options = this.generateOptions(values.split( this.stringSpliter));
+        this.options = this.generateOptions(values.split(this.stringSpliter));
     }
 
     // maximum options in one column for long list of checkbox options  
     @api maxOptionsPerColumn = MAX_OPTIONS_PER_COLUMN;
 
-    @api 
+    @api
     get customLabelClass() {
-        return this.__customLabelClass?'checkbox-label':'';
-    } 
+        return this.__customLabelClass ? 'checkbox-label' : '';
+    }
     set customLabelClass(value) {
         this.__customLabelClass = value;
     }
-    @api 
-    get customOptionClass () {
-        return this.__customOptionClass?'checkbox-option':'';
-    } 
-    set customOptionClass (value) {
+    @api
+    get customOptionClass() {
+        return this.__customOptionClass ? 'checkbox-option' : '';
+    }
+    set customOptionClass(value) {
         this.__customOptionClass = value;
     }
-    @api 
-    get customRadioClass () {
-        return this.__customRadioClass?'radio-option':'';
-    } 
-    set customRadioClass (value) {
+    @api
+    get customRadioClass() {
+        return this.__customRadioClass ? 'radio-option' : '';
+    }
+    set customRadioClass(value) {
         this.__customRadioClass = value;
     }
 
-    get isOptions () {
+    get isOptions() {
         return this.options?.length ?? 0;
     }
     get isCheckbox() {
@@ -86,7 +86,7 @@ export default class CustomCheckbox extends LightningElement {
     }
     get longOptionListClass() {
         const { maxOptionsPerColumn, options } = this;
-        return maxOptionsPerColumn && maxOptionsPerColumn < ( options?.length ?? 0 ) ? "checkbox-options-grid":"";
+        return maxOptionsPerColumn && maxOptionsPerColumn < (options?.length ?? 0) ? "checkbox-options-grid" : "";
     }
     get checkBoxClasses() {
         return `${this.customLabelClass} ${this.customOptionClass} ${this.longOptionListClass}`;
@@ -100,25 +100,54 @@ export default class CustomCheckbox extends LightningElement {
     }
     handleMaxGridrows() {
         const style = document.body.style;
-        style.setProperty( '--max-grid-rows', this.maxOptionsPerColumn)
+        style.setProperty('--max-grid-rows', this.maxOptionsPerColumn)
     }
-    generateOptions ( arrayOfValues = null ) {
+    generateOptions(arrayOfValues = null) {
         this.value = null;
-        return arrayOfValues 
+        return arrayOfValues
             ? [...({
-                    strOptions: [...arrayOfValues],
-                    [Symbol.iterator]: function* () {
-                        for( let option of this.strOptions ) yield { label: option, value: option };
-                    }
-              })]
+                strOptions: [...arrayOfValues],
+                [Symbol.iterator]: function* () {
+                    for (let option of this.strOptions) yield { label: option, value: option };
+                }
+            })]
             : [];
     }
     handleChange({ detail }) {
         const { value } = detail;
         const strSelectedValues = this.isCheckbox ? value.join(this.stringSpliter) : value;
 
-        Object.assign(detail, { strSelectedValues } );
-        Object.assign(this, { value, strSelectedValues } );
+        Object.assign(detail, { strSelectedValues });
+        Object.assign(this, { value, strSelectedValues });
     }
+
+    @api
+    validate() {
+        const isValid = false
+
+        if (this.required) {
+            let componentName = ''
+            if (this.isCheckbox) {
+                componentName = 'lightning-checkbox-group'
+            } else if (this.isRadio) {
+                componentName = 'lightning-radio-group'
+            } else if (this.isDropdown) {
+                componentName = 'lightning-combobox'
+            }
+
+            if (!componentName) {
+                return true;
+            }
+
+            isValid = [...this.template.querySelectorAll(componentName)].reduce((validSoFar, field) => {
+                return validSoFar && field.checkValidity();
+            }, true);
+            return { isValid, errorMessage: 'field is required' };
+        }
+        else {
+            return true;
+        }
+    }
+
 }
 export { TYPES }
